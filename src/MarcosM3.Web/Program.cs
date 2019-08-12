8bcb1eeae056862dbac7c79a9cbf9b3e4077c4dd
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-08-12 12:18:10 -0500

    Se agrega ApplicationInsight

--- src/MarcosM3.Web/Program.cs
+++ src/MarcosM3.Web/Program.cs
@@ -19,6 +19,7 @@ namespace MarcosM3.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
