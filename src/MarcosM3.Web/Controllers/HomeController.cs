921f8825f973c60e5e00125689245d7627d4a76e
Juan Benjamin Camacho <juan.camachocastro@dxc.com>
2019-12-27 00:54:12 -0600

    Se agrega formulario para determinar el precio de un marco

--- src/MarcosM3.Web/Controllers/HomeController.cs
+++ src/MarcosM3.Web/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MarcosM3.Web.Models;
 using Microsoft.AspNetCore.Authorization;
+using MarcosM3.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MarcosM3.Web.Controllers
 {
@@ -14,10 +16,12 @@ namespace MarcosM3.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MarcosM3DbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MarcosM3DbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -28,6 +32,17 @@ namespace MarcosM3.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Cotizacion()
+        {
+            var model = new CotizacionViewModel() { Cantidad = 1 };
+            model.Molduras = _context.Molduras.Select(moldura => new SelectListItem()
+            {
+                Text = moldura.Nombre,
+                Value = $"{moldura.Desperdicio},{moldura.Precio}"
+            }).ToList();
+            return View(model);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/MarcosM3.Web/Models/CotizacionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MarcosM3.Core;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MarcosM3.Web.Models
+{
+    public class CotizacionViewModel
+    {
+        public int Cantidad { get; set; }
+
+        public int Largo { get; set; }
+
+        public int Ancho { get; set; }
+
+        public Moldura Moldura { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Desperdicio { get; set; }
+        public bool ConMacocel { get; set; }
+
+        public List<SelectListItem> Molduras { get; set; }
+    }
+}
